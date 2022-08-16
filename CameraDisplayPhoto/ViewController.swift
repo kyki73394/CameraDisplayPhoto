419278cc2447d177d79ee7a06202fed4a5274117
Hannah Kim <kyki73394@gmail.com>
2022-08-15 21:34:01 -0400

    Completed function of "choose from library" and "take selfie" button--image appears on initial view controller.

--- CameraDisplayPhoto/ViewController.swift
+++ CameraDisplayPhoto/ViewController.swift
@@ -7,12 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
+
+    
+    //choose photo from Photo Library on device
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //choose to take a picture on device
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+    
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //show the photo in the image view
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            
+            //dismiss image picker view to see home screen again
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
+
 
 
 }
